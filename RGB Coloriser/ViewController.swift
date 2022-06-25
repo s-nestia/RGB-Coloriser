512c3bfdf31ca133b8d487d0ba14ab0e0d708c17
s-nestia <s.nestia@gmail.com>
2022-06-25 23:39:12 +0300

    create coloriser

--- RGB Coloriser/ViewController.swift
+++ RGB Coloriser/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private var colorView: UIView!
+    
+    @IBOutlet private var redColorSlider: UISlider!
+    @IBOutlet private var greenColorSlider: UISlider!
+    @IBOutlet private var blueColorSlider: UISlider!
+    
+    @IBOutlet private var redColorParameter: UILabel!
+    @IBOutlet private var greenColorParameter: UILabel!
+    @IBOutlet private var blueColorParameter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        redColorParameter.text = "0.00"
+        greenColorParameter.text = "0.00"
+        blueColorParameter.text = "0.00"
     }
 
-
+    @IBAction private func redColorChange(_ sender: UISlider) {
+        redColorParameter.text = String(format: "%.2f", redColorSlider.value)
+    }
+    
+    @IBAction private func greenColorChange(_ sender: UISlider) {
+          greenColorParameter.text = String(format: "%.2f", greenColorSlider.value)
+    }
+    
+    @IBAction private func blueColorChange(_ sender: UISlider) {
+          blueColorParameter.text = String(format: "%.2f", blueColorSlider.value)
+    }
+    
+    @IBAction private func viewColorChange(_ sender: UISlider) {
+        let color = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1.0
+        )
+        colorView.backgroundColor = color
+    }
 }
 
